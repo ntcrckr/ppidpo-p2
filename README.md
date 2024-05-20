beb513314b97f29b4095565713b0b6e19a3eb8df
Alexei Pozdnyakov <alexei.pozdnyakov@mail.ru>
2024-05-20 23:07:53 +0300

    completed hw 03 in Kotlin

--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Client-server currency rate provider
 
 To start run the following gradle configs:
 ```
+client-server -> eureka -> application -> bootRun
+client-server -> discovery -> application -> bootRun
 client-server -> server -> application -> bootRun
 client-server -> client -> application -> bootRun
+```
+
+To test discovery service make request:
+```
+GET http://localhost:8000/currencyRate
 ```
--- discovery/src/main/kotlin/org/ntcrckr/ppidpo2/discovery/DiscoveryController.kt
+++ discovery/src/main/kotlin/org/ntcrckr/ppidpo2/discovery/DiscoveryController.kt
@@ -1,8 +1,5 @@
 package org.ntcrckr.ppidpo2.discovery
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cloud.client.loadbalancer.LoadBalanced
-import org.springframework.context.annotation.Bean
 import org.springframework.http.HttpMethod
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -10,13 +7,11 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
 
 @RestController
-class DiscoveryController {
-    @Bean
-    @LoadBalanced
-    fun restTemplate(): RestTemplate = RestTemplate()
-
+class DiscoveryController(
+    private val restTemplate: RestTemplate,
+) {
     @GetMapping("/currencyRate")
-    fun getRate(@Autowired restTemplate: RestTemplate): ResponseEntity<*> {
+    fun getRate(): ResponseEntity<*> {
         return restTemplate.exchange(
             /* url = */ "http://server/currencyRate",
             /* method = */ HttpMethod.GET,
